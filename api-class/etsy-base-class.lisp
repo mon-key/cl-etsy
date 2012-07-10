78d8a419787a4e8e9805c1adfaab3f0a5b11b19a
MON KEY <monkey@sandpframing.com>
2012-07-10 17:31:56 -0400

    We now have an abstract base class in file api-class/etsy-base-class.lisp

---
+++ api-class/etsy-base-class.lisp
@@ -0,0 +1,13 @@
+;;; :FILE-CREATED <Timestamp: #{2012-07-10T17:09:19-04:00Z}#{12282} - by MON>
+;;; :FILE cl-etsy/api-class/etsy-base-class.lisp
+;;; ==============================
+
+(in-package #:cl-etsy)
+
+(defclass base-etsy ()
+  ()
+  ;; (:default-initargs )
+  (:documentation "An abstract class from which Etsy API classes should inherit."))
+
+;;; ==============================
+;;; EOF
