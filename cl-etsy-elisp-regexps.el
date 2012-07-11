f95e18759544bba292120f7de189902369de7125
MON KEY <monkey@sandpframing.com>
2012-07-11 13:21:36 -0400

    Add commented Common Lisp for writing skeleton files for each "type" defined by the Etsy v2 API

--- cl-etsy-elisp-regexps.el
+++ cl-etsy-elisp-regexps.el
@@ -53,6 +53,46 @@
 ;;                 finally (return (values syms gthr))))
 
 
+;; Common Lisp for writing skeleton files for each "type" defined by the Etsy v2 API
+;;
+;; (let ((base-dir #P"/<SOME-PATH-TO-CL-ETSY>/cl-etsy/api-class/")
+;;       (test-dir #P"/<SOME-PATH-TO-CL-ETSY>/cl-etsy/api-class/test/"))
+;;   ;; (ensure-directories-exist test-dir)
+;;   (loop 
+;;     for type in (list "Avatar" "BillCharge" "BillingOverview" "BillPayment" "Cart" "CartListing"
+;;                       "Category" "Country" "Coupon" "DataType" "FavoriteListing" "FavoriteUser"
+;;                       "FeaturedTreasury" "Feedback" "FeedbackInfo" "ForumPost" "Listing"
+;;                       "ListingImage" "ListingTranslation" "Order" "ParamList" "PaymentTemplate"
+;;                       "Receipt" "Region" "ShippingInfo" "ShippingTemplate" "ShippingTemplateEntry"
+;;                       "Shop" "ShopSection" "ShopSectionTranslation" "ShopTranslation" "Style" "Tag"
+;;                       "Team" "Transaction" "Treasury" "TreasuryCounts" "TreasuryListing"
+;;                       "TreasuryListingData" "User" "UserAddress" "UserProfile")
+;;     for convert = (nstring-downcase
+;;                    (nsubstitute #\- #\_ 
+;;                                 (cl-ppcre:regex-replace-all "(?<!^)((?<!_)([A-Z]))" type "-\\1")))
+;;     for name = (concatenate 'string "etsy-" convert "-class")
+;;     for base-path = (merge-pathnames (make-pathname :name name :type "lisp")
+;;                                      base-dir)
+;;     for test-path = (merge-pathnames (make-pathname :name name :type "lisp")
+;;                                      test-dir)
+;;     for url  = (format nil "(URL `http://www.etsy.com/developers/documentation/reference/~A')" (string-downcase type))
+;;     when (unless (probe-file base-path)
+;;            (with-open-file (f test-path 
+;;                               ;; :if-exists :error
+;;                               ;; :if-exists :rename
+;;                               :if-exists :supersede
+;;                               :if-does-not-exist :create
+;;                               :direction :output
+;;                               )
+;;              (mon:write-file-header f)
+;;              (format f "~%#|~2% ~A~2%----~%API-ASSOCIATIONS~2%----~%API-METHODS~2%|#~2%(in-package #:cl-etsy)~2%" url)
+;;              (format f 
+;;                      ";; ~A~%(defclass ~A (base-etsy)~%  (~%   )~%  ;; (:default-initargs )~%  (:documentation \"~% ~A\"))~2%;;; ==============================~%;;; EOF~%"
+;;                      type convert url)
+;;              t))
+;;     collect (list type convert name test-path)))
+
+
 ;;; ==============================
 ;;; EOF
 
