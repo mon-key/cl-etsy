ae34a7d92c08c5f2a71d8c8fb01c65fe2d15b165
MON KEY <monkey@sandpframing.com>
2012-07-12 17:06:18 -0400

    Add comment that param-list is really an api-type esp. as only the api-method class has resources associated with it.

--- api-class/etsy-param-list-class.lisp
+++ api-class/etsy-param-list-class.lisp
@@ -2,10 +2,18 @@
 ;;; :FILE cl-etsy/api-class/etsy-param-list-class.lisp
 ;;; ==============================
 
+#|
 
-;; (URL `http://www.etsy.com/developers/documentation/reference/paramlist')
-;; This is an associative array (an alist) that maps parameter names to types. 
-;; It has a varying number of fields with differing names.
+ (URL `http://www.etsy.com/developers/documentation/reference/paramlist')
+
+AFAICT the only resource that has a fields of this type is api-method.
+
+ params   - An array of method parameters and types.
+ defaults - An array of default values for parameters. Parameters that lack a default are required.
+ 
+We should probably move this further up the class lattice.
+
+|#
 
 (in-package #:cl-etsy)
 
@@ -20,14 +28,14 @@
     ;; :type string
     :documentation "Each field in the record represents an Etsy API method parameter.
 The value corresponds to one of the documented data or resource types."))
+
   ;; (:default-initargs :param-name nil)
+
   (:documentation "
-Each field in the record represents an Etsy API method parameter. 
-The value corresponds to one of the documented data or resource types. 
+This is an associative array (an alist, plist, or hash-table) that maps parameter names to types. 
+It has a varying number of fields with differing names.
  (URL `http://www.etsy.com/developers/documentation/reference/paramlist')"))
 
-;; (defmethod print-object ((x param-list) stream)
-;;   (dumb-printing stream x "~~A" param-name))
 
 ;;; ==============================
 ;;; EOF
