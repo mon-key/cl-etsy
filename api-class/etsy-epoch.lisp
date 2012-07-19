9a61ccde2c0aa0ae646d6d27113fa32f406446b0
MON KEY <monkey@sandpframing.com>
2012-07-19 19:28:40 -0400

    Add example api calls for "getServerEpoch" and "ping" API methods.

--- api-class/etsy-epoch.lisp
+++ api-class/etsy-epoch.lisp
@@ -10,7 +10,38 @@
   (incf value 2209006800) ;; convert to universal time
   (multiple-value-bind (second minute hour date month year)
       (decode-universal-time value)
-    (format nil "~D/~D/~D ~D:~D:~D" month date year hour minute second)))
+    (format nil "~D/~D/~D ~D:~D:~D" year month date  hour minute second)))
+
+;; (epoch-to-timestring 1342731989)
+
+#|
+
+  | "getServerEpoch"
+  | (yason:parse (api-call (concatenate 'string *base-url* "/server/epoch")) :object-as :alist)
+  | => (("pagination") ("type" . "Int") ("params") ("results" 1342731989) ("count" . 1))
+
+   ((:NAME        . "getServerEpoch")
+    (:DESCRIPTION . "Get server time, in epoch seconds notation.")
+    (:URI         . "/server/epoch")
+    (:PARAMS)
+    (:DEFAULTS)
+    (:TYPE         . "Int")
+    (:VISIBILITY   . "public")
+    (:HTTP-METHOD  . "GET"))
+
+  | "ping"
+  | (yason:parse (api-call (concatenate 'string *base-url* "/server/ping")) :object-as :alist)
+  | => (("pagination") ("type" . "String") ("params") ("results" "pong") ("count" . 1))
+
+  ((:NAME        . "ping")
+   (:DESCRIPTION . "Check that the server is alive.")
+   (:URI         . "/server/ping")
+   (:PARAMS)
+   (:DEFAULTS)
+   (:TYPE         . "String")
+   (:VISIBILITY   . "public")
+   (:HTTP-METHOD  . "GET"))
+
 
 ;;; ==============================
 ;;; EOF
